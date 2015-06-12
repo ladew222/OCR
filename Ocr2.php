6bc9d81e3c32abbf4ff1de4a208be3f7e53ad83b
ladew222 <egweinbe@gmail.com>
2015-06-12 17:15:27 -0500

    Start

--- Ocr2.php
+++ Ocr2.php
@@ -17,19 +17,19 @@ while ((($row = $result->fetch_assoc()) !== null)  && x<45  ) {
 
         $x++;
         $file="/var/www/html/OCR/Final/" . $row["page_name"]  . "_". $row["rec_type"]  . "_" .  $row["page_num"] . "_" .  $row["row_origin"] ."_" . $row["col_origin"] .".tif";
-        echo($file);
+        //echo($file);
 
        $witoutExt= basename($file, '.tif');
 
         if ($row["rec_type"]=="q") {
-           // $lastline = exec("tesseract " .$file  . " " . $withoutExt . "nobatch smdigits");
+            $lastline = exec("tesseract " .$file  . " " . $withoutExt . "nobatch smdigits");
 
         }
         else{
-          //  $lastline = exec("tesseract " .$file  . " " . $withoutExt);
+            $lastline = exec("tesseract " .$file  . " " . $withoutExt);
         }
        // echo $lastline;
-        echo ("<BR><BR> ::" . "tesseract " .$file  . " " );
+        echo ("<BR><BR> ::" . "tesseract " .$file  . " " . "/var/www/html/OCR/Final/". $row["page_name"]  );
         $filetxt = mysql_escape_string (nl2br(file_get_contents($file )));
 
         $sql = "UPDATE item SET text='$filetxt' WHERE id=" .$row["ID"];
