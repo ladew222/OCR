6897c6667e57be87f501b78d35d13e3f5a9cbf58
ladew222 <gopre222>
2015-06-16 00:48:29 -0500

    intercept added

--- DeskewAll.sh
+++ DeskewAll.sh
@@ -4,17 +4,25 @@ DIRECTORY=/var/www/html/OCR/Out
 
 
 
-FILES=/var/www/html/OCR/Out/*.jpg
+FILES=/var/www/html/OCR/Tif/*.tif
 x=1;
 for f in $FILES
 do
-  x=$((x+1))
- s=${f##*/}
-  t=${s%%.*}
-  u=".tif"
-  v=$t$u
- convert $f  -deskew 40  /var/www/html/OCR/Final/$v
-echo "convert  $f  /var/www/html/OCR/Final/$v"
+echo "aaa"
+if [[ $FILES == *"q"* ]]
+ then
+   x=$((x+1))
+   s=${f##*/}
+   t=${s%%.*}
+   u=".tif"
+   v=$t$u
+   convert $f  -deskew 40  /var/www/html/OCR/NoLines/$v
+   echo "convert  $f  /var/www/html/OCR/NoLines/$v"
+   sleep 1
+ fi
+
+
+
 done
 echo $x
 
