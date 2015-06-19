e5be4424c2a352d99a68f1e7af525268e717838a
ladew222 <gopre222>
2015-06-19 07:41:21 -0500

    intercept added

--- RemoveLines.sh
+++ RemoveLines.sh
@@ -3,12 +3,18 @@ DIRECTORY=/var/www/html/OCR/Out
 
 
 x=1
-for i in $DIRECTORY/*_q_*.JPG; do
+for i in $DIRECTORY/*; do
     # Process $i
 	#echo "converting $i to "
 	#echo "$i" | tr  q p
 	x=$((x+1))
-    convert $i -morphology close:1 "1x9:0,1,1,1,1,1,1,1,0" "$i" | tr  q p
+	if [[ $i =~ [q] ]]; then
+    echo "$i is a digit"
+         convert $i -morphology close:1 "1x7:0,1,1,1,1,1,0" "$i" | tr  q p
+    else
+        echo "oops"
+    fi
+
 done
 echo $x
 
