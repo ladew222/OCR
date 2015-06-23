e762cbbbe760eaec5425ba4881cb42b4299a2236
ladew222 <gopre222>
2015-06-23 01:05:22 -0500

    intercept added

--- test.php
+++ test.php
@@ -9,48 +9,70 @@
 */
 
 
-function str_to_address($context) {
-    $array = explode(" ", $context);
-    $array_reversed = array_reverse($array);
-    $numKey = "";
-    $zipKey = "";
-    foreach($array_reversed as $k=>$str) {
-        if($zipKey) { continue; }
-        if(strlen($str)===5 && is_numeric($str)) {
-            $zipKey = $k;
-        }
-    }
-    $array_reversed = array_slice($array_reversed, $zipKey);
-    $array = array_reverse($array_reversed);
-    foreach($array as $k=>$str) {
-        if($numKey) { continue; }
-        if(strlen($str)>1 && strlen($str)<6 && is_numeric($str)) {
-            $numKey = $k;
-        }
-
-    }
-    $array = array_slice($array, $numKey);
-    $string = implode(' ', $array);
-    return $string;
-}
-
 
 function str_to_address2($context) {
-    $array = explode("/n", $context);
+    $array = explode("\n", $context);
     $array_reversed = array_reverse($array);
+   // print_r($array_reversed);
     $numKey = "";
     $zipKey = "";
+    $foundziplinE ="";
+    $newstr="";
     $x=0;
+  //  echo('gfgf');
     foreach($array_reversed as $k=>$str) {
         $xx=0;
-        foreach($array_reversed as $k=>$str) {
+     //   echo('Looking at Line:');
+    //      echo($x);
+   //       echo('<BR>');
+        $array2 = explode(" ",$str);
+        $pos = strpos($str, "/");
+        foreach($array2 as $k=>$str2) {
+           //  echo('bb--<div style="color:blue">');
+          //    echo($str2);
+          //    echo("</div>");
             if ($zipKey) {
                 continue;
             }
-            if (strlen($str) === 5 && is_numeric($str)) {
+            if (strlen($str2) === 5 && is_numeric($str2) && $pos == 0) {
+                echo("Found:");
                 $zipKey = $k;
                 //found zip
                 $foundzipline = $x;
+                echo('Looking at Line:');
+                         echo($x);
+                         echo(":::");
+                         echo($str2);
+                         echo("<BR>");
+                $arr = array(
+                     trim($array_reversed[$x+3]),
+                     trim($array_reversed[$x+2]),
+                     trim($array_reversed[$x+1]),
+                    trim( $array_reversed[$x+0])
+                );
+                $y=0;
+                $fline="";
+                foreach ($arr as &$value) {
+               // echo strlen("Hello world!");
+                   if (strlen($value)>7){
+                         echo(" <div style='color:pink' >found line at: "  . $y . "::" . strlen($value) . "::". $value . "</div>");
+                        if(!($fline)){
+                            $fline=$yy;
+
+                        }
+                        $y++;
+                    }
+                    $yy++;
+                }
+                if ($y<3){
+
+                   echo ("Split at:" . $arr[$fline]);
+                }
+
+
+                $newstr= "<BR>1:".   $array_reversed[$x+3] . "2:". "<BR>". $array_reversed[$x+2] ."<BR>3:". $array_reversed[$x+1] . "<BR>" ."4:". $array_reversed[$x];
+                echo("<BR><div style='color:green'>" .$newstr ."</div><BR>");
+                print_r($array_reversed);
                 $xx++;
             }
             $xx++;
@@ -70,5 +92,56 @@ function str_to_address2($context) {
     $string = implode(' ', $array);
     return $string;
 }
+
+
+$con=mysqli_connect("localhost","root","gopre222","people");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+// Perform queries
+$result=mysqli_query($con,"SELECT text FROM item where ID=6988");
+
+$row = mysql_fetch_assoc($result); // fetch a result row
+print_r($row);
+//echo $row['text']; // output one of the result row's data fields.
+//echo("---");
+
+
+
+
+
+
+$link = mysqli_connect("localhost", "root", "gopre222", "people");
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$query = "SELECT text FROM item where col_origin=0";
+
+if ($result = mysqli_query($link, $query)) {
+
+    /* fetch associative array */
+    $x=0;
+    while ($row = mysqli_fetch_assoc($result)) {
+        //printf ("%s (%s)\n", $row["text"], $row["text"]);
+        echo("<div style='color:brown'> processing row:" .$x . ":val:". $row["text"] ."::</div>");
+        echo(str_to_address2($row["text"]));
+
+    $x++;
+    }
+
+    /* free result set */
+    mysqli_free_result($result);
+}
+
+/* close connection */
+mysqli_close($link);
+
 
 ?>
